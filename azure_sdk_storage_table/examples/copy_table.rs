364e2692fc85a30c35ab72a056ba4d9f1c53e58a
Cameron Taggart <cameron.taggart@gmail.com>
2019-09-24 23:23:39 +0200

    copy the entities using serde_json::Value

--- azure_sdk_storage_table/examples/copy_table.rs
+++ azure_sdk_storage_table/examples/copy_table.rs
@@ -3,21 +3,9 @@ use azure_sdk_storage_table::table::TableService;
 use futures::future::*;
 use std::error::Error;
 use tokio_core::reactor::Core;
-use chrono::{DateTime, TimeZone, Utc};
-#[macro_use]
-extern crate serde_derive;
 
 fn main() {
     code().unwrap();
-}
-
-#[allow(non_snake_case)]
-#[derive(Serialize, Deserialize)]
-struct Entity {
-    PartitionKey: String,
-    RowKey: String,
-    // Timestamp: DateTime<Utc>, TODO possible?
-    Timestamp: String,
 }
 
 // We run a separate method to use the elegant quotation mark operator.
@@ -26,8 +14,8 @@ fn code() -> Result<(), Box<dyn Error>> {
     // First we retrieve the account names and master keys from environment variables.
     let account = std::env::var("STORAGE_ACCOUNT").expect("Set env variable STORAGE_ACCOUNT first!");
     let master_key = std::env::var("STORAGE_MASTER_KEY").expect("Set env variable STORAGE_MASTER_KEY first!");
-    // let to_account = std::env::var("TO_STORAGE_ACCOUNT").expect("Set env variable STORAGE_ACCOUNT first!");
-    // let to_master_key = std::env::var("TO_STORAGE_MASTER_KEY").expect("Set env variable STORAGE_MASTER_KEY first!");
+    let to_account = std::env::var("TO_STORAGE_ACCOUNT").expect("Set env variable TO_STORAGE_ACCOUNT first!");
+    let to_master_key = std::env::var("TO_STORAGE_MASTER_KEY").expect("Set env variable TO_STORAGE_MASTER_KEY first!");
 
     let table_name = std::env::args()
         .nth(1)
@@ -36,16 +24,20 @@ fn code() -> Result<(), Box<dyn Error>> {
     let mut core = Core::new()?;
 
     let table_service = TableService::new(Client::new(&account, &master_key)?);
-    // let to_table_service = TableService::new(Client::new(&to_account, &to_master_key)?);
+    let to_table_service = TableService::new(Client::new(&to_account, &to_master_key)?);
 
-    let future = table_service.query_entities(&table_name, None).and_then(move |entities: Vec<Entity>| {
-        println!("Table {} has {} entities", &table_name, entities.len());
-        // for entity in entities {
-        //     to_table_service.insert_entity(&table_name, entity); // it is a future...
-        // }
-        Ok(())
+    println!("creating table {}", &table_name);
+    let create_table = to_table_service.create_table(&table_name);
+    core.run(create_table)?;
+
+    let copy_entities = table_service.query_entities(&table_name, None).and_then(move |entities: Vec<serde_json::Value>| {
+        println!("copying {} entities to table {}", entities.len(), &table_name);
+        let inserts = entities.into_iter().map(move |entity| {
+            to_table_service.insert_entity(&table_name, &entity)
+        });
+        join_all(inserts)
     });
+    core.run(copy_entities)?;
 
-    core.run(future)?;
     Ok(())
 }
