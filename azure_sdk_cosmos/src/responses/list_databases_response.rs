30210196ea94e5f09b599e15f93203b35dc733b2
Francesco Cogno <francesco.cogno@outlook.com>
2020-01-19 16:41:57 +0100

    completed list databases (response and builder)

--- azure_sdk_cosmos/src/responses/list_databases_response.rs
+++ azure_sdk_cosmos/src/responses/list_databases_response.rs
@@ -2,7 +2,7 @@ use crate::database::Database;
 use crate::from_headers::*;
 use crate::ResourceQuota;
 use azure_sdk_core::errors::AzureError;
-use azure_sdk_core::session_token_from_headers;
+use azure_sdk_core::{continuation_token_from_headers_optional, session_token_from_headers};
 use chrono::{DateTime, Utc};
 use http::HeaderMap;
 
@@ -14,9 +14,13 @@ pub struct ListDatabasesResponse {
     pub activity_id: uuid::Uuid,
     pub charge: f64,
     pub session_token: String,
-    pub last_change: DateTime<Utc>,
+    pub last_state_change: DateTime<Utc>,
     pub resource_quota: Vec<ResourceQuota>,
     pub resource_usage: Vec<ResourceQuota>,
+    pub schema_version: String,
+    pub service_version: String,
+    pub continuation_token: Option<String>,
+    pub gateway_version: String,
 }
 
 impl std::convert::TryFrom<(&HeaderMap, &[u8])> for ListDatabasesResponse {
@@ -44,9 +48,13 @@ impl std::convert::TryFrom<(&HeaderMap, &[u8])> for ListDatabasesResponse {
             charge: request_charge_from_headers(headers)?,
             activity_id: activity_id_from_headers(headers)?,
             session_token: session_token_from_headers(headers)?,
-            last_change: last_state_change_from_headers(headers)?,
+            last_state_change: last_state_change_from_headers(headers)?,
             resource_quota: resource_quota_from_headers(headers)?,
             resource_usage: resource_usage_from_headers(headers)?,
+            schema_version: schema_version_from_headers(headers)?,
+            service_version: service_version_from_headers(headers)?,
+            continuation_token: continuation_token_from_headers_optional(headers)?,
+            gateway_version: gateway_version_from_headers(headers)?,
         })
     }
 }
