f77defb94424d051f4f799ebb8cae24451b2f6ff
Francesco Cogno <francesco.cogno@outlook.com>
2020-06-04 19:52:30 +0200

    list users

--- azure_sdk_cosmos/src/requests/list_users_builder.rs
+++ azure_sdk_cosmos/src/requests/list_users_builder.rs
@@ -3,10 +3,11 @@ use crate::responses::ListUsersResponse;
 use crate::ResourceType;
 use azure_sdk_core::errors::{check_status_extract_headers_and_body, AzureError};
 use azure_sdk_core::prelude::*;
+use futures::stream::{unfold, Stream};
 use hyper::StatusCode;
 use std::convert::TryInto;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct ListUsersBuilder<'a, 'b, C>
 where
     C: CosmosClient,
@@ -17,6 +18,22 @@ where
     consistency_level: Option<ConsistencyLevel<'b>>,
     continuation: Option<&'b str>,
     max_item_count: i32,
+}
+
+impl<'a, 'b, C> Clone for ListUsersBuilder<'a, 'b, C>
+where
+    C: CosmosClient,
+{
+    fn clone(&self) -> Self {
+        Self {
+            database_client: self.database_client,
+            user_agent: self.user_agent,
+            activity_id: self.activity_id,
+            consistency_level: self.consistency_level.clone(),
+            continuation: self.continuation,
+            max_item_count: self.max_item_count,
+        }
+    }
 }
 
 impl<'a, 'b, C> ListUsersBuilder<'a, 'b, C>
@@ -221,5 +238,46 @@ where
         .await?;
 
         Ok((&headers, &body as &[u8]).try_into()?)
+    }
+
+    pub fn stream(&self) -> impl Stream<Item = Result<ListUsersResponse, AzureError>> + '_ {
+        #[derive(Debug, Clone, PartialEq)]
+        enum States {
+            Init,
+            Continuation(String),
+        };
+
+        unfold(
+            Some(States::Init),
+            move |continuation_token: Option<States>| {
+                async move {
+                    debug!("continuation_token == {:?}", &continuation_token);
+                    let response = match continuation_token {
+                        Some(States::Init) => self.execute().await,
+                        Some(States::Continuation(continuation_token)) => {
+                            self.clone()
+                                .with_continuation(&continuation_token)
+                                .execute()
+                                .await
+                        }
+                        None => return None,
+                    };
+
+                    // the ? operator does not work in async move (yet?)
+                    // so we have to resort to this boilerplate
+                    let response = match response {
+                        Ok(response) => response,
+                        Err(err) => return Some((Err(err), None)),
+                    };
+
+                    let continuation_token = match &response.continuation_token {
+                        Some(ct) => Some(States::Continuation(ct.to_owned())),
+                        None => None,
+                    };
+
+                    Some((Ok(response), continuation_token))
+                }
+            },
+        )
     }
 }
