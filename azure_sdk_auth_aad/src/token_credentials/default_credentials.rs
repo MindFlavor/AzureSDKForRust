cc8b1cccaa8c582808b374d89c97a3d2d41e2321
Damien Pontifex <damien.pontifex@gmail.com>
2020-07-04 18:10:12 +0800

    Default credentials

---
+++ azure_sdk_auth_aad/src/token_credentials/default_credentials.rs
@@ -0,0 +1,81 @@
+use crate::token_credentials::AzureCliCredential;
+use crate::{token_credentials::TokenCredential, EnvironmentCredential};
+use azure_sdk_core::errors::AzureError;
+use log::debug;
+use oauth2::AccessToken;
+
+pub struct DefaultCredentialBuilder {
+    include_environment_credential: bool,
+    include_cli_credential: bool,
+}
+
+impl DefaultCredentialBuilder {
+    pub fn new() -> Self {
+        DefaultCredentialBuilder {
+            include_cli_credential: true,
+            include_environment_credential: true,
+        }
+    }
+
+    pub fn exclude_environment_credential(&mut self) -> &mut Self {
+        self.include_environment_credential = false;
+        self
+    }
+    pub fn exclude_cli_credential(&mut self) -> &mut Self {
+        self.include_cli_credential = false;
+        self
+    }
+    pub fn build(&self) -> DefaultCredential {
+        let source_count: usize =
+            self.include_cli_credential as usize + self.include_cli_credential as usize;
+        let mut sources =
+            Vec::<Box<dyn TokenCredential + Send + Sync>>::with_capacity(source_count);
+        if self.include_environment_credential {
+            sources.push(Box::new(EnvironmentCredential {}));
+        }
+        if self.include_cli_credential {
+            sources.push(Box::new(AzureCliCredential {}));
+        }
+        DefaultCredential::with_sources(sources)
+    }
+}
+
+pub struct DefaultCredential {
+    sources: Vec<Box<dyn TokenCredential + Send + Sync>>,
+}
+
+impl DefaultCredential {
+    pub fn with_sources(sources: Vec<Box<dyn TokenCredential + Send + Sync>>) -> Self {
+        DefaultCredential { sources }
+    }
+}
+
+impl Default for DefaultCredential {
+    fn default() -> Self {
+        DefaultCredential {
+            sources: vec![
+                Box::new(EnvironmentCredential {}),
+                Box::new(AzureCliCredential {}),
+            ],
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl TokenCredential for DefaultCredential {
+    async fn get_token(&self, resource: &str) -> Result<Box<AccessToken>, AzureError> {
+        for source in &self.sources {
+            let token_res = source.get_token(resource).await;
+
+            if let Ok(token) = token_res {
+                return Ok(token);
+            } else {
+                debug!("Failed to get credentials: {:?}", token_res.err().unwrap());
+            }
+        }
+
+        Err(AzureError::GenericErrorWithText(
+            "End of default list".to_owned(),
+        ))
+    }
+}
--- azure_sdk_auth_aad/src/token_credentials/mod.rs
+++ azure_sdk_auth_aad/src/token_credentials/mod.rs
@@ -1,6 +1,8 @@
 mod cli_credentials;
+mod default_credentials;
 mod environment_credentials;
 pub use crate::token_credentials::cli_credentials::*;
+pub use crate::token_credentials::default_credentials::*;
 pub use crate::token_credentials::environment_credentials::*;
 use azure_sdk_core::errors::AzureError;
 use oauth2::AccessToken;
