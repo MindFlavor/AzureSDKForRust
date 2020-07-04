fd81c431d2129c663b83ad41dcd4a89df7897257
Damien Pontifex <damien.pontifex@gmail.com>
2020-07-04 19:20:15 +0800

    MSI

--- azure_sdk_auth_aad/src/token_credentials/mod.rs
+++ azure_sdk_auth_aad/src/token_credentials/mod.rs
@@ -1,9 +1,11 @@
 mod cli_credentials;
 mod default_credentials;
 mod environment_credentials;
+mod managed_identity_credentials;
 pub use crate::token_credentials::cli_credentials::*;
 pub use crate::token_credentials::default_credentials::*;
 pub use crate::token_credentials::environment_credentials::*;
+pub use crate::token_credentials::managed_identity_credentials::*;
 use azure_sdk_core::errors::AzureError;
 use oauth2::AccessToken;
 
